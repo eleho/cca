2e8a167f31747437c53533dc6c8c0ff9c51d9e28
eleho <ed.eleho@gmail.com>
2020-06-19 16:01:15 -0500

    Initial commit of lecture 2

---
+++ cca02.py
@@ -0,0 +1,24 @@
+i = 1
+j = 2
+print(i + j)
+print(i * j)
+print(i / j) # ans with print is a int
+print(i % j)
+print(i == 1)
+print(i == 2)
+print(i != 0)
+i += 1
+print(i)
+print(i % 2 != 0 and j % 2 != 0)
+print(i % 2 == 0 or j % 2 == 0)
+i = 1
+print(i % 2 == 0 or j % 2 == 0)
+j = 3
+print(i % 2 == 0 or j % 2 == 0)
+print(not i % 2 == 0)
+l = [1, 2, 3, 4]
+i = 3
+print(i in l)
+i = 5
+print(i in l)
+print(i not in l)
